8723a2902519fcb04b1a22dd653fe7dc22f929d6
shock59 <sam@bladeinteractive.com.au>
2022-06-13 09:58:02 +1000

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const element = document.getElementById("usercount");
+
+async function usercount() {
+  const response = await fetch("https://scratchaddons.com/usercount.json");
+  const json = await response.json();
+
+  console.log(json);
+
+  element.innerHTML = json.count;
+}
+
+setInterval(usercount(), 20000);
