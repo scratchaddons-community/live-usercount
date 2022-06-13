fe81897b911925a7c9531a1083596e41a9af26fa
shock59 <sam@bladeinteractive.com.au>
2022-06-13 10:35:43 +1000

    fix

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 const element = document.getElementById("usercount");
 
 async function usercount() {
-  const response = await fetch("https://scratchaddons.com/usercount.json");
-  const json = await response.json();
+  let response = await fetch("https://scratchaddons.com/usercount.json");
+  let json = await response.json();
 
   console.log(json);
 
   element.innerHTML = json.count;
 }
 
-setInterval(usercount(), 20000);
+setInterval(usercount, 20000);
