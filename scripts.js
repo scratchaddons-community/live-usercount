bc40bb3463386db2bac070d1b23eb9ac168a6f38
RedGuy12 <paul@reid-family.org>
2022-08-15 08:26:23 -0500

    Fix #1

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 const element = document.getElementById("usercount");
 
 async function usercount() {
-  let response = await fetch("https://scratchaddons.com/usercount.json");
+  let response = await fetch("https://scratchaddons.com/usercount.json?date="+Date.now());
   let json = await response.json();
 
   console.log(json);
 
-  element.innerHTML = json.count;
+  element.innerText = json.count;
 }
-
-setInterval(usercount, 20000);
+usercount()
+setInterval(usercount, 30_000);
